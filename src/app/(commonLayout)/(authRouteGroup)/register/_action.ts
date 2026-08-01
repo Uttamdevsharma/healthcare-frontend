1324081202c6e6128a8f4310ebf1f86364ca24ae
Uttam <uttamrohit4545@gmail.com>
2026-08-01 21:51:20 +0600

    chore : auth.types

--- src/app/(commonLayout)/(authRouteGroup)/register/_action.ts
+++ src/app/(commonLayout)/(authRouteGroup)/register/_action.ts
@@ -1,11 +1,10 @@
 "use server";
 
 import { httpClient } from "@/lib/axios/httpClient";
-import { IUser } from "@/types/auth.types";
 
 export const registerAction = async (payload: any) => {
   try {
-    const response = await httpClient.post<{ success: boolean; message?: string; data?: IUser }>(
+    const response = await httpClient.post<{ success: boolean; message?: string; data?: any }>(
       "/auth/register",
       payload
     );
